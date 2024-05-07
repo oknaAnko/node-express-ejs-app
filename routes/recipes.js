99e18d94449f6c499c1fd36a08658103f364235a
oknaAnko <anna.jedrzejko@op.pl>
2024-05-07 20:12:17 +0200

    Update recipe route

--- routes/recipes.js
+++ routes/recipes.js
@@ -13,7 +13,7 @@ const Recipe = require('../models/recipe');
 //   }
 // ]
 
-// All recipes
+// Show all recipes
 router.get('/', cors(), async (req, res) => {
   try {
     const recipes = await Recipe.find({})
@@ -24,7 +24,7 @@ router.get('/', cors(), async (req, res) => {
   }
 })
 
-// New recipe
+// Create new recipe
 router.post('/', cors(), async (req, res) => {
   const recipe = new Recipe({
     title: req.body.title,
@@ -36,19 +36,46 @@ router.post('/', cors(), async (req, res) => {
 
   try {
     const newRecipe = await recipe.save()
-    console.log(req.body)
-    res.send('Success!')
+    res.status(200).json({
+      success: true,
+      redirectUrl: `/recipes/${newRecipe._id}`
+      // redirect to: `http://localhost:3032/recipes/${newRecipe._id}`
+    })
 
   } catch (error) {
     console.log('err', error)
   }
 })
 
-// Single recipe
-router.get('/:id', async (req, res) => {
+// Show single recipe
+router.get('/:id', cors(), async (req, res) => {
   try {
     const recipe = await Recipe.findById(req.params.id)
     res.json(recipe)
+
+  } catch (error) {
+    console.log('err', error)
+  }
+})
+
+// Update recipe
+router.put('/:id', cors(), async (req, res) => {
+  let recipe;
+
+  try {
+    recipe = await Recipe.findById(req.params.id)
+    recipe.title = req.body.title
+    recipe.time = req.body.time
+    recipe.difficulty = req.body.difficulty
+    recipe.category = req.body.category
+    recipe.description = req.body.description
+
+    const updatedRecipe = await recipe.save()
+    res.status(200).json({
+      success: true,
+      redirectUrl: `/recipes/${updatedRecipe._id}`
+      // redirect to: `http://localhost:3032/recipes/${updatedRecipe._id}`
+    })
 
   } catch (error) {
     console.log('err', error)
--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
 
